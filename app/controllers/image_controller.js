c1c33b221e1a57f0fc5d41939a2da73573ed4b55
kokhow <[chuangkokhow@gmail.com]>
2022-03-30 11:17:25 +0800

    fixed bugs on endpoints

--- app/controllers/image_controller.js
+++ app/controllers/image_controller.js
@@ -1,14 +1,14 @@
 "use strict";
 
-import mongoose from 'mongoose';
-import { logger } from '../config/logger';
-import Image from '../models/image';
+import mongoose from "mongoose";
+import { logger } from "../config/logger";
+import Image from "../models/image";
 
-const env = process.env.NODE_ENV || 'development';
+const env = process.env.NODE_ENV || "development";
 
 /**
  * List all images
- * @query {string} username 
+ * @query {string} username
  * @returns {array} user
  */
 export async function listImage(req, res, next) {
@@ -16,30 +16,30 @@ export async function listImage(req, res, next) {
   const offset = parseFloat(req.query.offset) || 1;
 
   Image.find({})
-    .sort({ 'createdAt': -1 })
+    .sort({ createdAt: -1 })
     .lean()
     .limit(limit)
     .skip((offset - 1) * limit)
     .exec(function (err, record) {
       if (err) {
-        logger.error('Unable to list image in image_controller. Error: ' + err);
+        logger.error("Unable to list image in image_controller. Error: " + err);
 
         return res.status(500).json({
-          errorCode: 'ERR_INTERNAL_SERVER_ERROR',
-          errorMessage: 'Could not list image'
+          errorCode: "ERR_INTERNAL_SERVER_ERROR",
+          errorMessage: "Could not list image",
         });
       }
 
       res.status(200).json({
-        images: (record && record.length > 0) ? record : [],
-        total: record.length
+        images: record && record.length > 0 ? record : [],
+        total: record.length,
       });
     });
 }
 
 /**
  * Save images
- * @query {string} username 
+ * @query {string} username
  * @returns {array} user
  */
 export async function saveImage(req, res, next) {
@@ -53,14 +53,32 @@ export async function saveImage(req, res, next) {
 
   newImage.save(function (err) {
     if (err) {
-      logger.error('Unable to save new image in image_controller. Error: ' + err);
+      logger.error(
+        "Unable to save new image in image_controller. Error: " + err
+      );
 
       return res.status(500).json({
-        errorCode: 'ERR_INTERNAL_SERVER_ERROR',
-        errorMessage: 'Could not save new image'
+        errorCode: "ERR_INTERNAL_SERVER_ERROR",
+        errorMessage: "Could not save new image",
       });
     }
 
     res.status(200).end();
   });
+}
+
+export async function importImage(req, res, next) {
+  const axios = require("axios");
+
+  axios
+    .get(
+      "https://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline"
+    )
+    .then((res) => {
+      console.log(`statusCode: ${res.status}`);
+      console.log(res);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 }
--- app/routes/image.js
+++ app/routes/image.js
@@ -1,12 +1,13 @@
 "use strict";
 
-import * as express from 'express';
-import * as imageController from '../controllers/image_controller';
+import * as express from "express";
+import * as imageController from "../controllers/image_controller";
 
-const env = process.env.NODE_ENV || 'development';
+const env = process.env.NODE_ENV || "development";
 const router = express.Router();
 
-router.get('/images/', imageController.listImage);
-router.post('/image/', imageController.saveImage);
+router.get("/images/import", imageController.importImage);
+router.get("/images/", imageController.listImage);
+router.post("/image/", imageController.saveImage);
 
 export default router;
